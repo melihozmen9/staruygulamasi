31f7064f0209a5dc3d0939adabdec05b31285ce8
melihozmen9 <105754659+melihozmen9@users.noreply.github.com>
2022-07-18 00:13:21 +0300

    staruygulamasi1

--- star uygulaması/ViewController.swift
+++ star uygulaması/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+   
+    
+    @IBOutlet weak var mytableview: UITableView!
+    var kullanicilar = [kullanici]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loaddata()
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kullanicilar.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    func loaddata () {
+        let kullanici1 = kullanici()
+        kullanici1.firstname = "Ronaldinho"
+        kullanici1.lastname = "Gaúcho"
+        kullanici1.profilphoto = UIImage(named: "rondilho")!
+        kullanicilar.append(kullanici1)
+        
+        let kullanici2 = kullanici()
+        kullanici2.firstname = "Christiano"
+        kullanici2.lastname = "Ronaldo"
+        kullanici2.profilphoto = UIImage(named: "Cristiano_Ronaldo_2018")!
+        kullanicilar.append(kullanici2)
+        
+        let kullanici3 = kullanici()
+        kullanici3.firstname = "Lionel"
+        kullanici3.lastname = "Messi"
+        kullanici3.profilphoto = UIImage(named: "Lionel_Messi_20180626")!
+        kullanicilar.append(kullanici3)
+        
+        let kullanici4 = kullanici()
+        kullanici4.firstname = "Diego"
+        kullanici4.lastname = "Maradona"
+        kullanici4.profilphoto = UIImage(named: "diego-maradona-1401100569-36")!
+        kullanicilar.append(kullanici4)
+        
+        
     }
 
-
 }
+
 
---
+++ star uygulaması/kullanici.swift
@@ -0,0 +1,14 @@
+//
+//  kullanici.swift
+//  star uygulaması
+//
+//  Created by Kullanici on 17.07.2022.
+//
+
+import Foundation
+import UIKit
+class kullanici {
+    var firstname = " "
+    var lastname = " "
+    var profilphoto = UIImage()
+}
